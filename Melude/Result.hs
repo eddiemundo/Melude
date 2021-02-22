ea56a6f87d3ba5763c174d64c0fc82c3054efd7d
Jon Shen <shenjonathan0@gmail.com>
2021-02-22 00:35:47 -0500

    add singleton error constructors for Result

--- Melude/Result.hs
+++ Melude/Result.hs
@@ -12,6 +12,7 @@ import Data.Function ((&))
 import Data.Foldable (Foldable(fold))
 import Data.Sequence (Seq)
 import qualified Data.Sequence as Seq
+import Melude.NonEmptySeq (NESeq((:<||)))
 
 type NonEmptySeq a = NESeq a
 
@@ -22,6 +23,15 @@ data Result e a
   = Failures !(NonEmptySeq (Failure e))
   | Success a
   deriving Functor
+
+-- pattern Fail :: e -> CallStack -> Result e a
+-- pattern Fail e cs = Failures ((Failure (Just cs) e) :<|| Seq.Empty)
+
+fromErrorWithCallStack :: e -> CallStack -> Result e a
+fromErrorWithCallStack error stack = error & Failure (Just stack) & NonEmptySeq.singleton & Failures
+
+fromError :: e -> Result e a
+fromError error = error & Failure Nothing & NonEmptySeq.singleton & Failures
 
 toErrors :: Result e a -> Seq e
 toErrors (Failures failures) = failures <&> failureToError & NonEmptySeq.toSeq
